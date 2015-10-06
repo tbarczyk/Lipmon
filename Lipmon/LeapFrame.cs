79d41df2ad0ad97d27d3b4de111eb372a67d4641
tbarczyk <barczyktomek@gmail.com>
2015-10-05 18:38:41 -0700

    added first stuff

---
+++ Lipmon/LeapFrame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lipmon
+{
+    public class LeapFrame
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public int X { get { return _x; } set { _x = value; } }
+        public int Y { get { return _y; } set { _y = value; } }
+        public int Z { get { return _z; } set { _z = value; } }
+    }
+}
---
+++ Lipmon/MainWindow.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lipmon
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(12, 12);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(152, 63);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(12, 81);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(152, 62);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(422, 168);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(125, 62);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 560);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Connect);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
